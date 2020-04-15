419da48b3a74fea70e24822be2d5040f39db53db
HaiLe81 <lekhachai9999@gmail.com>
2020-04-15 21:56:25 +0700

    example Dynamic class names

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import './App.css';
-import TodoItem from './components/TodoItem';
-import Nav from './components/Nav/Nav';
-import Login from './components/Login/Login';
+import TodoItem from './components/TodoItem/TodoItem';
+// import Nav from './components/Nav/Nav';
+// import Login from './components/Login/Login';
 
 function App() {
+
+  let todoItem = [
+    { title: 'Hai dep Try', isComplete: false },
+    { title: "Hai Hoc Gioi", isComplete: true },
+    { title: 'Hai moi mua xe moi', isComplete: false }
+  ]
+
   return (
     <div className="App">
+      { todoItem.length > 0 && todoItem.map((item, index) => <TodoItem key={index} item={item} />) }
+      { todoItem.length === 0 && 'Nothing Here' }
         {/* <TodoItem/> */}
-        <Nav/>
-        <Login/>
+        {/* <Nav/> */}
+        {/* <Login/> */}
     </div>
   );
 }

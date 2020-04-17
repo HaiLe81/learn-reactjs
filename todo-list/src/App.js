6c6c7c3ea462d97f6f1f88231219a53b40a01e0c
HaiLe81 <lekhachai9999@gmail.com>
2020-04-17 15:43:44 +0700

    update css

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TodoItem from './components/TodoItem/TodoItem';
 // import Nav from './components/Nav/Nav';
 // import Login from './components/Login/Login';
+import tick from './images/down-arrow.svg';
 
 function App() {
   let todoItem = [
@@ -11,7 +12,8 @@ function App() {
     { title: 'Hai moi mua xe moi', isComplete: true }
   ]
   let [state, setState] = useState({ todoItem });
-  
+  let [newItem, setNewItem] = useState({ newItem: '' });
+
   function onChangeStatusItem(item, index) {
     const todoItems = state.todoItem
     const isComplete = item.isComplete
@@ -22,19 +24,66 @@ function App() {
           ...item,
           isComplete: !isComplete
         },
-        ...todoItems.slice(index+1)
+        ...todoItems.slice(index + 1)
       ]
     })
-    console.log('item after', state)
   }
 
+  function onKeyUp(e) {
+    let text = e.target.value;
+    if (e.keyCode === 13) { // Enter key
+      if (!text) {
+        return;
+      }
+      text = text.trim()
+      if (!text) { return; }
+
+
+      setState({
+        todoItem: [
+          {
+            title: text, isComplete: false
+          },
+          ...state.todoItem
+        ]
+      })
+      setNewItem({
+        newItem: ''
+      })
+    }
+  }
+
+  function onChange(e){
+    setNewItem({
+      newItem: e.target.value
+    })
+  }
+
+  function onChangeStatusAll() {
+
+  }
 
   return (
     <div className="App">
+      <div className="Header">
+        <img onClick={onChangeStatusAll} src={tick} width={32} height={32} alt="not found" />
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={newItem.newItem}
+          onChange={onChange}
+          onKeyUp={onKeyUp} />
+      </div>
       {state.todoItem.length > 0 && state.todoItem.map((item, index) =>
         <TodoItem key={index} item={item} onClick={() => onChangeStatusItem(item, index)} />)}
       {state.todoItem.length === 0 && 'Nothing Here'}
-      {/* <TodoItem/> */}
+      <div className="Footer">
+        <p className="item">count selected</p>
+        <button className="item">All</button>
+        <button className="item">Active</button>
+        <button className="item">Completed</button>
+        <div className="item"/>
+      </div>
       {/* <Nav/> */}
       {/* <Login/> */}
     </div>
--- todo-list/src/components/TodoItem/TodoItem.js
+++ todo-list/src/components/TodoItem/TodoItem.js
@@ -1,15 +1,23 @@
 import React from 'react'
 import './TodoItem.css';
 import classnames from 'classnames';
+import checkImg from '../../images/right.svg';
+import checkComleteImg from '../../images/correct.svg';
 
 let TodoItem = (props) => {
 
     const { item, onClick } = props;
 
+    let url = checkImg;
+    if(item.isComplete){
+        url = checkComleteImg
+    }
+
     return (
-        <div onClick={onClick} className={classnames('TodoItem', {
+        <div className={classnames('TodoItem', {
             'TodoItem-complete': item.isComplete
         })}>
+            <img onClick={onClick} src={url}  height={32} width={32} alt="not found"/>
             <p>{props.item.title}</p>
         </div>
     );
